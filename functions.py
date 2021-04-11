4a17ab222224a3f8fda5291bd0416b6ee82c92ad
NightFox001 <johnnyh961@gmail.com>
2021-04-11 02:14:32 -0500

    Created vis functions and way to store/access q values

--- functions.py
+++ functions.py
@@ -1,32 +1,27 @@
 from random import randint
-import time
 
 def getValidActions(world, x, y, has_block):
     directions = ['north', 'south', 'west', 'east']
-    return_ops = []
+    state_action = world[(x, y)]['action']
+    state_blocks = world[(x, y)]['no_of_blocks']
+    validActions = []
 
     if has_block:
-        if world[(x, y)]['action'] == "d" and world[(x, y)]['no_of_blocks'] < 4:
-            return_ops.append("d")
-            print("DELIVERED! (", x, y, ') has', world[(x, y)]['no_of_blocks'] + 1, 'blocks')
-            time.sleep(1)
-            return return_ops
-        else:
-            for d in directions:
-                if world[(x, y)][d] != 'nan':
-                    return_ops.append(d)
-                    
-    # does not have block
-    else:
-        if world[(x, y)]['action'] == "p" and world[(x, y)]['no_of_blocks'] > 0:
-            return_ops.append("p")
-            print("PICKED UP! (", x, y, ') has', world[(x, y)]['no_of_blocks'] - 1, 'blocks left')
-            time.sleep(1)
-            return return_ops
+        if state_action == "d" and state_blocks < 4: return ["d"]
         for d in directions:
             if world[(x, y)][d] != 'nan':
-                return_ops.append(d)
-    return return_ops
+                validActions.append(d)
+        return validActions
+    # does not have block
+    if state_action == "p" and state_blocks > 0: return ["p"]
+    for d in directions:
+        if world[(x, y)][d] != 'nan':
+            validActions.append(d)
+    return validActions
+
+def getNextCoords(action, world, x, y):
+    if (action == 'd' or action == 'p'): return [x, y]
+    return world[(x, y)][action]
 
 def chooseRandomAction(valid_actions):
     if (valid_actions[0] == "d"): return ["d", 13]
@@ -42,3 +37,55 @@ def actionNumber(action):
     if action == 'p': n = 4
     if action == 'd': n = 5
     return n
+
+def coordsNotValid(x, y, w, h):
+    if x > w or x < 0 or y > h or y < 0:
+        print('coords are out of bound')
+        return True
+
+
+def getStates(w, h):
+    rows, cols = (h, w)
+    matrix=[]
+    count = 0
+    for i in range(rows):
+        col = []
+        for j in range(cols):
+            col.append(count)
+            count += 1
+        matrix.append(col)
+    return matrix
+
+# probablly gonna make something better 
+# (update: i did, see 'getStates')
+def getStateFromCoords(x, y):
+    if (x == 0):
+        if (y == 0 ): return 0
+        if (y == 1 ): return 1
+        if (y == 2 ): return 2
+        if (y == 3 ): return 3
+        if (y == 4 ): return 4
+    if (x == 1):
+        if (y == 0 ): return 5
+        if (y == 1 ): return 6
+        if (y == 2 ): return 7
+        if (y == 3 ): return 8
+        if (y == 4 ): return 9
+    if (x == 2):
+        if (y == 0 ): return 10
+        if (y == 1 ): return 11
+        if (y == 2 ): return 12
+        if (y == 3 ): return 13
+        if (y == 4 ): return 14
+    if (x == 3):
+        if (y == 0 ): return 15
+        if (y == 1 ): return 16
+        if (y == 2 ): return 17
+        if (y == 3 ): return 18
+        if (y == 4 ): return 19
+    if (x == 4):
+        if (y == 0 ): return 20
+        if (y == 1 ): return 21
+        if (y == 2 ): return 22
+        if (y == 3 ): return 23
+        if (y == 4 ): return 24
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 from functions import * 
-from random import randint
+from vis_funcs import * 
 from Visualize.visualize import Visualize as vl
 from WorldProcessing.readWorld import ReadWorld
 
@@ -13,17 +13,15 @@ global has_block
 global world
 
 x = 0
-y = 0
+y = 4
 drop_off_loc = [(0, 0), (4, 0), (2, 2), (4, 4)]
 pick_up_loc = [(1, 3), (4, 2)]
 has_block = False
 h, w, world = ReadWorld().fill_world('testworld.txt')
 
+alpha = 0.5     # learning rate
+gamma = 0.2     # discount rate
 
-def getNextCoords(action):
-    global world
-    if (action == 'd' or action == 'p'): return [x, y]
-    return world[(x, y)][action]
 
 def applyAction(action):
     global x
@@ -37,31 +35,15 @@ def applyAction(action):
         if world[(x, y)]['no_of_blocks'] == 4:
             drop_off_loc.remove((x, y))
             print((x, y), 'is full')
-            time.sleep(1)
         has_block = False
     elif action == 'p':
         world[(x, y)]['no_of_blocks'] -= 1
         if world[(x, y)]['no_of_blocks'] == 0:
             pick_up_loc.remove((x, y))
             print((x, y), 'is empty')
-            time.sleep(1)
         has_block = True
 
-def actionNumber(action):
-    n = 0
-    if action == 'north':
-        n = 0
-    elif action == 'south':
-        n = 1
-    elif action == 'west':
-        n = 2
-    elif action == 'east':
-        n = 3
-    elif action == 'p':
-        n = 4
-    elif action == 'd':
-        n = 5
-    return n
+
 
 def updateQTable(q_table, world, x, y, reward, alpha, gamma, action, app_op):
     q_values_future_state = []
@@ -83,7 +65,7 @@ def main():
 
     next_x = -1
     next_y = -1
-    FIRST_STEPS = 50
+    FIRST_STEPS = 500
     SECOND_STEPS = 5500
 
     policy = "PRANDOM"
@@ -95,42 +77,19 @@ def main():
     q_size = q_offset * 2
     q_table = np.zeros((q_size, 6))
 
-    print('Hello')
+    # build world in starting state
     world_vl = vl()
     world_vl.visualize_gen(h, w, world)
+    initWorld(h, w, world, world_vl, x, y)
 
-    for r in range(h):
-        for c in range(w):
-            if world[(c, r)]['action'] == 'd':
-                world_vl.fill_block(world[(c, r)]['coor'], 'green')
-            if world[(c, r)]['action'] == 'p':
-                world_vl.fill_block(world[(c, r)]['coor'], 'blue')
-            world_vl.write_block(world[(c, r)]['coor'], world[(c, r)]['action'], 'white')
-            world_vl.write_block(world[(c, r)]['coor'], str(world[(c, r)]['coor']), 'black', 'n', 15)
-
-
-    # q_table = np.zeros((w*2, h, 6))
-    action = ['n', 's', 'w', 'e', 'p', 'd']
-
-
-    q_table[x+1, 2] = 2
-    q_table[(x+2), 3] = 3
-    print(q_table)
-    return
-
-
-    world_vl.fill_block(world[(x, y)]['coor'], 'yellow')
-    world_vl.write_block(world[(x, y)]['coor'], 'start', 'black')
-
+    
     # always use PRANDOM for first 500 steps
-    for i_d in range(0, FIRST_STEPS):
-        print(i_d, '{:.2f}%'.format((i_d / FIRST_STEPS)*100))
-        print('location: ', x, y)
+    for step in range(0, FIRST_STEPS):
+        print(step, '{:.2f}%'.format((step / FIRST_STEPS)*100))
 
-        # check for out of bound values
-        if x > max(world)[0] or x < 0 or y > max(world)[1] or y < 0:
-            print('Out of bound')
-            break
+        # this "state" is used for q_table
+        state = getStateFromCoords(x, y)
+        if (has_block): state += q_offset
 
         # list of all possible operations at current state
         valid_actions = getValidActions(world, x, y, has_block)
@@ -142,47 +101,28 @@ def main():
         print("action:", action)
 
         applyAction(action)
-        next_x, next_y = getNextCoords(action)
+        next_x, next_y = getNextCoords(action, world, x, y)
 
         # update qtable
+        # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s’,a’) — Q(s,a)]
 
+        # q_table[state, action] = q_table[state, action] + lr * (reward + gamma * np.max(q_table[new_state, :]) — q_table[state, action])
         
-
         x = next_x
         y = next_y
 
-        world_vl.fill_block(world[(x, y)]['coor'], 'red')
-        world_vl.write_block(world[(x, y)]['coor'], str(i_d), pos='s')
-        # print("reward:", reward)
-
         # if terminal state reached
         if len(pick_up_loc) == 0 and len(drop_off_loc) == 0:
+            print("\nTerminal state reached")
             break
 
-    for r in range(h):
-        for c in range(w):
-            if world[(c, r)]['action'] == 'd':
-                world_vl.fill_block(world[(c, r)]['coor'], 'green')
-                world_vl.write_block(world[(c, r)]['coor'], str(world[(c, r)]['no_of_blocks']), 'black', pos='s')
-            if world[(c, r)]['action'] == 'p':
-                world_vl.fill_block(world[(c, r)]['coor'], 'blue')
-                world_vl.write_block(world[(c, r)]['coor'], str(world[(c, r)]['no_of_blocks']), 'black', pos='s')
-            world_vl.write_block(world[(c, r)]['coor'], world[(c, r)]['action'], 'white')
-            world_vl.write_block(world[(c, r)]['coor'], str(world[(c, r)]['coor']), 'black', 'n', 15)
+        # break if the next x or y is not valid (should never happen)
+        if (coordsNotValid(x, y, w, h)): break
 
+    updateWorld(h, w, world, world_vl)
 
+    print("\ncompleted in", step+1, "steps")
 
-    # apply 'step' aka action
-    # set new_state and reward from that state
-
-
-# chose policy for next 5500 steps
-# for _ in range(0, SECOND_STEPS):
-
-
-# action = np.argmax(q_table[state, :])
-# for e in world[(0, 0)]:
-#     print(world[(0, 0)][e])
 
 
 if __name__ == '__main__':
