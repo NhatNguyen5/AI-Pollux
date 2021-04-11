1480182c410d4b2f8afe69edfad886a534c6f669
NightFox001 <johnnyh961@gmail.com>
2021-04-10 22:42:43 -0500

    refactoring

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import numpy as np
 from random import randint
-import cv2 as cv
+# import cv2 as cv
 import time
-
 from Visualize.visualize import Visualize as vl
 from WorldProcessing.readWorld import ReadWorld
+
+global x
+global y
+global drop_off_loc
+global pick_up_loc
+global has_block
+global world
+
+x = 0
+y = 4
+drop_off_loc = [(0, 0), (4, 0), (2, 2), (4, 4)]
+pick_up_loc = [(1, 3), (4, 2)]
+has_block = False
+h, w, world = ReadWorld().fill_world('testworld.txt')
 
 
 # World:
@@ -22,7 +35,7 @@ from WorldProcessing.readWorld import ReadWorld
 # put_x((x, y), 'color') // put a big X on the block
 
 
-def getAppOps(world, x, y, has_block):
+def getValidActions(world, x, y, has_block):
     directions = ['north', 'south', 'west', 'east']
     return_ops = []
 
@@ -45,103 +58,113 @@ def getAppOps(world, x, y, has_block):
         for d in directions:
             if world[(x, y)][d] != 'nan':
                 return_ops.append(d)
-
     return return_ops
 
 
-def chooseRandom(app_operators):
-    if (app_operators[0] == "d"): return ["d", 13]
-    if (app_operators[0] == "p"): return ["p", 13]
-    index = randint(0, len(app_operators) - 1)
-    return [app_operators[index], -1]
+def chooseRandomAction(valid_actions):
+    if (valid_actions[0] == "d"): return ["d", 13]
+    if (valid_actions[0] == "p"): return ["p", 13]
+    index = randint(0, len(valid_actions) - 1)
+    return [valid_actions[index], -1]
 
+def getNextCoords(action):
+    global world
+    if (action == 'd' or action == 'p'): return [x, y]
+    return world[(x, y)][action]
 
+def applyAction(action):
+    global x
+    global y
+    global drop_off_loc
+    global pick_up_loc
+    global has_block
+    global world
+    if action == 'd':
+        world[(x, y)]['no_of_blocks'] += 1
+        if world[(x, y)]['no_of_blocks'] == 4:
+            drop_off_loc.remove((x, y))
+            print((x, y), 'is full')
+            time.sleep(1)
+        has_block = False
+    elif action == 'p':
+        world[(x, y)]['no_of_blocks'] -= 1
+        if world[(x, y)]['no_of_blocks'] == 0:
+            pick_up_loc.remove((x, y))
+            print((x, y), 'is empty')
+            time.sleep(1)
+        has_block = True
+
+    
 def main():
-    print('Hello')
-    h, w, world = ReadWorld().fill_world('testworld.txt')
-    # print(world)
-    # for b in world:
-        # print(b, world[b]['east'])
-    world_vl = vl()
+    global x
+    global y
+    global drop_off_loc
+    global pick_up_loc
+    global has_block
+    global world
 
-    world_vl.visualize_gen(h, w, world)
-    # fill_block((0, 0), 'green')
-    # write_block((0, 0), 'Hello', 'white', 'n')
-    # put_x((0, 0), 'white')
-
-    for r in range(h):
-        for c in range(w):
-            if world[(c, r)]['action'] == 'd':
-                world_vl.fill_block(world[(c, r)]['coor'], 'green')
-            if world[(c, r)]['action'] == 'p':
-                world_vl.fill_block(world[(c, r)]['coor'], 'blue')
-            world_vl.write_block(world[(c, r)]['coor'], world[(c, r)]['action'], 'white')
-            world_vl.write_block(world[(c, r)]['coor'], str(world[(c, r)]['coor']), 'black', 'n', 15)
-
-    initial_state = (0, 0)
-
-    # print(world[0, 1]['action'])
-    q_table = np.zeros((w, h, 6))
-    # print(q_table)
-    action = ['n', 's', 'w', 'e', 'p', 'd']
-
-    #  start episode
-
-    FIRST_STEPS = 2000
+    next_x = -1
+    next_y = -1
+    FIRST_STEPS = 50
     SECOND_STEPS = 5500
+
     policy = "PRANDOM"
     # policy = "PEXPLOIT"
     # policy = "PGREEDY"
 
-    has_block = False
-    x = 0
-    y = 4
+    print('Hello')
+    world_vl = vl()
+    world_vl.visualize_gen(h, w, world)
+
+    for r in range(h):
+        for c in range(w):
+            if world[(c, r)]['action'] == 'd':
+                world_vl.fill_block(world[(c, r)]['coor'], 'green')
+            if world[(c, r)]['action'] == 'p':
+                world_vl.fill_block(world[(c, r)]['coor'], 'blue')
+            world_vl.write_block(world[(c, r)]['coor'], world[(c, r)]['action'], 'white')
+            world_vl.write_block(world[(c, r)]['coor'], str(world[(c, r)]['coor']), 'black', 'n', 15)
+
+    q_table = np.zeros((w, h, 6))
+    action = ['n', 's', 'w', 'e', 'p', 'd']
+
     world_vl.fill_block(world[(x, y)]['coor'], 'yellow')
     world_vl.write_block(world[(x, y)]['coor'], 'start', 'black')
 
-    pick_up_loc = [(1, 3), (4, 2)]
-    drop_off_loc = [(0, 0), (4, 0), (2, 2), (4, 4)]
-
     # always use PRANDOM for first 500 steps
-    # i_d = 0
     for i_d in range(0, FIRST_STEPS):
-    #while True:
         print(i_d, '{:.2f}%'.format((i_d / FIRST_STEPS)*100))
         print('location: ', x, y)
+
+        # check for out of bound values
         if x > max(world)[0] or x < 0 or y > max(world)[1] or y < 0:
             print('Out of bound')
             break
-        app_operators = getAppOps(world, x, y, has_block)
-        # print('applicable_operators:', app_operators)
 
-        chosen_op, reward = chooseRandom(app_operators)
-        print("action:", chosen_op)
-        if chosen_op == 'd':
-            world[(x, y)]['no_of_blocks'] += 1
-            if world[(x, y)]['no_of_blocks'] == 4:
-                drop_off_loc.remove((x, y))
-                print((x, y), 'is full')
-                time.sleep(1)
-            has_block = False
-        elif chosen_op == 'p':
-            world[(x, y)]['no_of_blocks'] -= 1
-            if world[(x, y)]['no_of_blocks'] == 0:
-                pick_up_loc.remove((x, y))
-                print((x, y), 'is empty')
-                time.sleep(1)
-            has_block = True
-        else:
-            t_x = world[(x, y)][chosen_op][0]
-            t_y = world[(x, y)][chosen_op][1]
-            x = t_x
-            y = t_y
-        # print('next location: ', x, y)
+        # list of all possible operations at current state
+        valid_actions = getValidActions(world, x, y, has_block)
+
+        # choose action based off policy
+        action, reward = chooseRandomAction(valid_actions)
+        # action, reward = chooseExploitAction(valid_actions)
+        # action, reward = chooseGreedyAction(valid_actions)
+        print("action:", action)
+
+        applyAction(action)
+        next_x, next_y = getNextCoords(action)
+
+        # update qtable
+
+        x = next_x
+        y = next_y
+
         world_vl.fill_block(world[(x, y)]['coor'], 'red')
         world_vl.write_block(world[(x, y)]['coor'], str(i_d), pos='s')
         # print("reward:", reward)
+
+        # if terminal state reached
         if len(pick_up_loc) == 0 and len(drop_off_loc) == 0:
             break
-        i_d += 1
 
     for r in range(h):
         for c in range(w):
@@ -156,7 +179,7 @@ def main():
 
 
 
-    # apply 'step' aka chosen_op
+    # apply 'step' aka action
     # set new_state and reward from that state
 
 
