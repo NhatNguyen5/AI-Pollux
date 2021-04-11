83512e57f7940ea4c4b9e32c8d735b979b6e7b70
NightFox001 <johnnyh961@gmail.com>
2021-04-11 14:37:38 -0500

    refactoring

--- main.py
+++ main.py
@@ -9,19 +9,15 @@ from WorldProcessing.readWorld import ReadWorld
 FIRST_STEPS = 500
 SECOND_STEPS = 5500
 
-global x
-x = 0
-global y
-y = 4
-global drop_off_loc
-drop_off_loc = [(0, 0), (4, 0), (2, 2), (4, 4)]
-global pick_up_loc
-pick_up_loc = [(1, 3), (4, 2)]
-global has_block
-has_block = False
-global world
-h, w, world = ReadWorld().fill_world('testworld.txt')
+global x,  y, drop_off_loc, pick_up_loc, has_block, done, world, q_offset, q_table
 
+x = 0
+y = 4
+drop_off_loc = [(0, 0), (4, 0), (2, 2), (4, 4)]
+pick_up_loc = [(1, 3), (4, 2)]
+has_block = False
+done = False
+h, w, world = ReadWorld().fill_world('testworld.txt')
 
 # states, given x and y, returns state index for q_table
 # get index with states[x][y]
@@ -29,10 +25,9 @@ states = getStates(w, h)
 
 # offset is diffence in q_table between row for a state, and the row for the same state when holding a block 
 q_offset = w * h
-q_size = q_offset * 2
-q_table = np.zeros((q_size, 6))
+q_table = np.zeros((q_offset*2, 6))
 
-alpha = 0.3     # learning rate
+alpha = 0.15     # learning rate
 gamma = 0.5     # discount rate
 epsilon = 0.8   # chance of being greedy in exploit policy
 
@@ -98,71 +93,10 @@ def greedyAction(state, valid_actions):
     if (valid_actions[0] == "p"): return ["p", 13]
     return [bestValidAction(state, valid_actions), -1]
 
+def makeSteps(steps, policy):
+    global x, y, drop_off_loc, pick_up_loc, has_block, done, world, q_offset, q_table
 
-def main():
-    global x
-    global y
-    global drop_off_loc
-    global pick_up_loc
-    global has_block
-    global world
-
-    done = False
-    next_x = -1
-    next_y = -1
-    
-    # policy = "PRANDOM"
-    # policy = "PEXPLOIT"
-    policy = "PGREEDY"
-
-    # build world in starting state
-    world_vl = vl()
-    world_vl.visualize_gen(h, w, world)
-    initWorld(h, w, world, world_vl, x, y)
-
-
-    # always use PRANDOM for first 500 steps
-    for step1 in range(0, FIRST_STEPS):
-        # print(step1, '{:.2f}%'.format((step1 / (FIRST_STEPS+SECOND_STEPS))*100))
-
-        # this "state" is used for q_table
-        state = states[x][y]
-        if (has_block): state += q_offset
-
-        # list of all possible operations at current state
-        valid_actions = getValidActions(world, x, y, has_block)
-        
-        action, reward = chooseRandomAction(valid_actions)
-        # print("action:", action)
-
-        # has_block gets updated here
-        applyAction(action)
-
-        next_x, next_y = getNextCoords(action, world, x, y)
-        next_valid_actions = getValidActions(world, next_x, next_y, has_block)        
-        next_state = states[next_x][next_y]
-        if (has_block): next_state += q_offset
-
-        # update qtable  
-        q_table[state][actionToIndex(action)] = (1-alpha)*Q(state, action) + alpha*(reward + gamma*maxQ(next_state, next_valid_actions))
-
-        # 'move' to the next state
-        x = next_x
-        y = next_y
-
-        # if terminal state reached
-        if len(pick_up_loc) == 0 and len(drop_off_loc) == 0:
-            print("\nTerminal state reached")
-            done = True
-            break
-
-        # break if the next x or y is not valid (should never happen)
-        if (coordsNotValid(x, y, w, h)): break
-
-    if (done): 
-    for step2 in range(0, SECOND_STEPS):
-        # print(step2, '{:.2f}%'.format(((step2+FIRST_STEPS)/(FIRST_STEPS+SECOND_STEPS))*100))
-
+    for step in range(0, FIRST_STEPS):
         # this "state" is used for q_table
         state = states[x][y]
         if (has_block): state += q_offset
@@ -177,19 +111,19 @@ def main():
         elif (policy == 'PGREEDY'):
             action, reward = greedyAction(state, valid_actions)
 
-        print("action:", action)
-
+        # print("action:", action)
 
         # has_block gets updated here
         applyAction(action)
 
         next_x, next_y = getNextCoords(action, world, x, y)
+        if (coordsNotValid(next_x, next_y, w, h)): break
         next_valid_actions = getValidActions(world, next_x, next_y, has_block)        
         next_state = states[next_x][next_y]
         if (has_block): next_state += q_offset
 
         # update qtable  
-        q_table[state][actionToIndex(action)] = (1-alpha)*Q(state, action) + alpha*(reward + gamma*maxQ(next_state, next_valid_actions))
+        q_table[state][actionToIndex(action)] = round((1-alpha)*Q(state, action) + alpha*(reward + gamma*maxQ(next_state, next_valid_actions)), 4)
 
         # 'move' to the next state
         x = next_x
@@ -198,18 +132,42 @@ def main():
         # if terminal state reached
         if len(pick_up_loc) == 0 and len(drop_off_loc) == 0:
             print("\nTerminal state reached")
+            done = True
             break
 
         # break if the next x or y is not valid (should never happen)
         if (coordsNotValid(x, y, w, h)): break
+    return step+1
 
+
+def main():
+    global world
+    
+    # policy = "PRANDOM"
+    policy = "PEXPLOIT"
+    # policy = "PGREEDY"
+
+    # build world in starting state
+    world_vl = vl()
+    world_vl.visualize_gen(h, w, world)
+    initWorld(h, w, world, world_vl, x, y)
+
+    # always use PRANDOM for first 500 steps
+    steps1 = makeSteps(FIRST_STEPS, 'PRANDOM')
+
+    print("q_table after 500 steps")
+    print(q_table)
+
+    # if terminal state was not reached in first 500 steps, keep going
+    steps2 = 0
+    if (done == False): 
+        steps2 = makeSteps(SECOND_STEPS, policy)
+
+    print("\n\nq_table after completed")
+    print(q_table)
+    print("\ncompleted in", steps1+steps2, "steps")
 
     updateWorld(h, w, world, world_vl)
-
-    print(q_table)
-    print("\ncompleted in", step1+1 + step2+1, "steps")
-
-
 
 if __name__ == '__main__':
     main()
